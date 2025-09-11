656648fd0464fd26cd2864c0ce9bf38507a1ff57
Bhargav V <87763304+imvbhargav@users.noreply.github.com>
2025-09-11 21:29:56 +0530

    Add code for 5. Longest Palindromic Substring

---
+++ 5-longest-palindromic-substring/solution.py
@@ -0,0 +1,31 @@
+class Solution(object):
+    def longestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        n = len(s)
+        if n < 2:
+            return s
+
+        # Use a list to store mutable values for start and max_len
+        state = [0, 1] # [start, max_len]
+
+        # Helper function to expand around a center
+        def expand_around_center(left, right):
+            # Access and modify the list elements
+            while left >= 0 and right < n and s[left] == s[right]:
+                current_len = right - left + 1
+                if current_len > state[1]: # state[1] is max_len
+                    state[1] = current_len
+                    state[0] = left      # state[0] is start
+                left -= 1
+                right += 1
+
+        for i in range(n):
+            # For odd length palindromes (e.g., "aba")
+            expand_around_center(i, i)
+            # For even length palindromes (e.g., "abba")
+            expand_around_center(i, i + 1)
+
+        return s[state[0]:state[0] + state[1]]
